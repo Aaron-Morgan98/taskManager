13b1a5ab80a74ee1b6e1b89147a7557fb8c687bc
Aaron-Morgan98 <aaron.morgan742@gmail.com>
2024-04-18 20:57:22 +0100

    fronend router set up + file structure and user register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import './styles/App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {CreateUser} from "./pages/CreateUser";
+
 
 function App() {
   return (
     <div>
+        <Router>
+            <Routes>
+                <Route path="/create_user" element={<CreateUser />} />
+            </Routes>
+        </Router>
     </div>
   );
 }
